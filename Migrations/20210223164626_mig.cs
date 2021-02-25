9a0463240470f53a6c595452d9f38eb18bfb52f7
Miles Paulson <milespaulson6@gmail.com>
2021-02-24 21:09:54 -0700

    yeet

---
+++ Migrations/20210223164626_mig.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Assignment5.Migrations
+{
+    public partial class mig : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Products",
+                columns: table => new
+                {
+                    BookID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Title = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    AuthorFirst = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    AuthorMiddle = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    AuthorLast = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Publisher = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    ISBN = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Classification = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Category = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Price = table.Column<double>(type: "float", nullable: false),
+                    PageLength = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Products", x => x.BookID);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Products");
+        }
+    }
+}
---
+++ Models/ViewModels/BookListViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Assignment5.Models.ViewModels
+{
+    public class BookListViewModel
+    {
+        public IEnumerable<Product> Products { get; set; }
+
+        public PagingInfo PagingInfo { get; set; }
+
+
+    }
+}
